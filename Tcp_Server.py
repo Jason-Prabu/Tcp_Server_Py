bb6921c039607cb61c417adfad26a522255b4c58
Jason Prabu. L <32505007+Jason-Prabu@users.noreply.github.com>
2017-10-17 00:36:01 +0530

    Create Tcp_Server.py

---
+++ Tcp_Server.py
@@ -0,0 +1,18 @@
+import socket
+
+TCP_IP = '192.168.1.5'           #enter your device IP
+TCP_PORT = 5005
+
+BUFFER_SIZE = 20                       # Normally 1024, but we want fast response
+while 1:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     #creating socket object with IPv4 packet
+    s.bind((TCP_IP, TCP_PORT))
+    s.listen(1)                                               #listen to connection from 1 client
+    conn, addr = s.accept()
+    print("Connection address:", addr)
+
+    data = conn.recv(BUFFER_SIZE)
+    if data:
+       print ("received data:", data)
+       conn.shutdown(socket.SHUT_WR)
+       conn.close()
